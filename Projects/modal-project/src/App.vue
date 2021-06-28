d0e04cb99c85aeffdf51f43db77e2831e0500671
Stephan Griesel <griesel.stephan@scotch-soda.com>
2021-06-28 18:19:08 +0200

    first project start

---
+++ Projects/modal-project/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <h1>{{ title }}</h1>
+  <input type="text" ref="name">
+  <!-- <button @click="handleClick">Click me!</button> -->
+
+  <Modal />
+</template>
+
+<script>
+
+import Modal from './components/Modal.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Modal
+  },
+  data() {
+    return {
+      title: 'My First Vue App, woohoooo!'
+    }
+  },
+  methods: {
+    handleClick(){
+      console.log(this.$refs.name)
+      this.$refs.name.classList.add('active')
+      this.$refs.name.focus()
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
