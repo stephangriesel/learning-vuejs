42d260709698226fb7fa45d35011b4c59beed92c
Stephan Griesel <griesel.stephan@scotch-soda.com>
2021-06-29 15:29:21 +0200

    scoped and global styles

--- Projects/modal-project/src/components/Modal.vue
+++ Projects/modal-project/src/components/Modal.vue
@@ -1,12 +1,13 @@
 <template>
     <div class="backdrop">
         <div class="modal">
+            <h1>Modal Title</h1>
             <p>Modal Content</p>
         </div>
     </div>
 </template>
 
-<style>
+<style scoped>/* scope like this <<< or just add to class like below  */
 .modal {
     width:400px;
     padding:20px;
@@ -20,5 +21,16 @@
     background:rgba(0,0,0,0.5);
     width:100%;
     height:100%;
+}
+h1 {
+    /* color:red; */
+}
+
+.modal h1 {
+    color:red; /* or scope styles like this */
+}
+
+.modal p {
+    color:black;
 }
 </style>
