94a0f2da33c9e7bf07846d0e4657b120524a6338
Stephan Griesel <griesel.stephan@scotch-soda.com>
2021-07-06 15:29:36 +0200

    add block component & logic

--- Projects/reaction-timer/src/App.vue
+++ Projects/reaction-timer/src/App.vue
@@ -1,20 +1,23 @@
 <template>
 <h1>Hello Reaction Timer</h1>
-<button @click="start">Play</button>
+<button @click="start" :disabled="isPlaying">Play</button>
+<Block v-if="isPlaying" :delay="delay"/>
 </template>
 
 <script>
 
+import Block from './components/Block.vue'
+
 export default {
   name: 'App',
-  components: {},
+  components: { Block },
   data() {
     return {
       isPlaying: false,
       delay: null
     }
   },
-  method: {
+  methods: {
     start(){
       this.delay = 2000 + Math.random() * 5000
       this.isPlaying = true,
@@ -29,8 +32,11 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #444;
   margin-top: 60px;
+  display:flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 }
 </style>
---
+++ Projects/reaction-timer/src/components/Block.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="block">
+      click me
+  </div>
+</template>
+
+<script>
+export default {
+    props: [
+        'delay'
+    ]
+
+}
+</script>
+
+<style>
+.block {
+    width:400px;
+    background:green;
+    border-radius:10px;
+    color:#FFF;
+    text-align: center;
+    margin:40px auto;
+    padding:40px;
+}
+</style>
