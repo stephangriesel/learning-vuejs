da4c749ec25557e49cf4e0eb680abf726c15f2ca
Stephan Griesel <griesel.stephan@scotch-soda.com>
2021-07-22 17:26:55 +0200

    delete function

--- Projects/web-form/src/components/SignUpForm.vue
+++ Projects/web-form/src/components/SignUpForm.vue
@@ -13,9 +13,9 @@
     </select>
 
     <label>Skills:</label>
-    <input type="text" v-model="tempSkill" @keyup.alt="addSkill">
+    <input type="text" v-model="tempSkill" @keyup="addSkill">
     <div v-for="skill in skills" :key="skill" class="pill">
-      {{ skill }}
+      <span @click="deleteSkill(skill)">{{ skill }}</span>
     </div>
 
     <div class="terms">
@@ -62,14 +62,19 @@ export default {
   methods: {
     addSkill(e){
       console.log("Add Skill Event: ", e)
-      if(e.key === ',' && this.tempSkill){
+      if(e.key === ' ' && this.tempSkill){
         if(!this.skills.includes(this.tempSkill)){
           this.skills.push(this.tempSkill)
         }
         this.tempSkill = ''
       }
+    },
+    deleteSkill(skill){
+      this.skills = this.skills.filter((item) => {
+        return skill !== item
+      })
     }
-  }
+  },
 }
 </script>
 
@@ -110,5 +115,17 @@ input[type="checkbox"]{
   position:relative;
   top:2px;
   }
+.pill {
+  display:inline-block;
+  margin:20px 10px 0 0;
+  padding: 6px 12px;
+  background:#EEE;
+  border-radius:20px;
+  font-size:12px;
+  letter-spacing: 1px;
+  font-weight: bold;
+  color:#777;
+  cursor:pointer;
+}
 
 </style>
