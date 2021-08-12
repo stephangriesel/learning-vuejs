c332e9b4d34b129a63e2f6a0e1bc50caaf685d8d
Stephan Griesel <griesel.stephan@scotch-soda.com>
2021-08-12 17:22:50 +0200

    folder structure

---
+++ Projects/vue-router/src/views/jobs/Jobs.vue
@@ -0,0 +1,26 @@
+<template>
+<div>
+  <h1>Jobs</h1>
+  <div v-for="job in jobs" :key="job.id">
+    <h2>{{ job.title }}</h2>
+  </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      jobs: [
+        {title: 'Ux Designer', id: 1, details: 'lorem'},
+        {title: 'Developer', id: 2, details: 'lorem'},
+        {title: 'Support', id: 3, details: 'lorem'}
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
