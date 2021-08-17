574faaa3976cb2f1c674342443d3aaa1c7043dcd
Stephan Griesel <griesel.stephan@scotch-soda.com>
2021-08-17 18:40:20 +0200

    setup, folders & routes

---
+++ Projects/nuxt-vue/pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <section class="container">
+    <!-- <a href="/users">Users</a> -->
+    <!-- <router-link></router-link> -->
+    <nuxt-link to="/users">Users</nuxt-link>
+  </section>
+</template>
+
+<script>
+export default {}
+</script>
