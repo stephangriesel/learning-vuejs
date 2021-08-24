e859c79bc71139a3c586e349ca961d0440c02aa5
Stephan <sgriesel@gmail.com>
2021-08-24 07:01:29 +0200

    adding links & navigating

--- Projects/nuxt-vue/pages/index.vue
+++ Projects/nuxt-vue/pages/index.vue
@@ -3,6 +3,7 @@
     <!-- <a href="/users">Users</a> -->
     <!-- <router-link></router-link> -->
     <nuxt-link to="/users">Users</nuxt-link>
+    <button @click="$router.push('/products')">Go to products</button>
   </section>
 </template>
 
---
+++ Projects/nuxt-vue/pages/products/_pid/index.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1>single product page (ID: {{ $route.params.pid}}) </h1>
+</template>
