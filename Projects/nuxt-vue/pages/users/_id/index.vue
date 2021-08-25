afa38432f6497dfb0b03a8f6327fe0c2c94ad6da
Stephan <sgriesel@gmail.com>
2021-08-25 06:12:28 +0200

    validate data

--- Projects/nuxt-vue/pages/users/_id/index.vue
+++ Projects/nuxt-vue/pages/users/_id/index.vue
@@ -1,3 +1,13 @@
 <template>
   <h1>single user, with ID: {{ $route.params.id }}</h1>
 </template>
+
+<script>
+export default {
+  validate(data) {
+    // validate data and set condition based on properties received
+    console.log(data);
+    return data.params.id == 1;
+  }
+};
+</script>
