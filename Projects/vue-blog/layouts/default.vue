b4e84488557f79868027c59e2be7b6fbafd3f496
Stephan <sgriesel@gmail.com>
2021-08-27 07:24:12 +0200

    header component

--- Projects/vue-blog/layouts/default.vue
+++ Projects/vue-blog/layouts/default.vue
@@ -1,8 +1,26 @@
 <template>
   <div>
+    <TheHeader @sidenavToggle="displaySidenav = !displaySidenav" />
+    <TheSideNav :show="displaySidenav" @close="displaySidenav = false" />
     <nuxt />
   </div>
 </template>
+
+<script>
+import TheHeader from "~/components/Navigation/TheHeader";
+import TheSideNav from "~/components/Navigation/TheSidenav";
+export default {
+  components: {
+    TheHeader,
+    TheSideNav
+  },
+  data() {
+    return {
+      displaySidenav: false
+    };
+  }
+};
+</script>
 
 <style>
 html {
