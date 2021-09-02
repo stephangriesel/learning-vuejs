2c534606733e8a7825d891c3585bd675833843a8
Stephan <sgriesel@gmail.com>
2021-09-02 06:43:16 +0200

    add styles

--- Projects/vue-blog/pages/admin/_postId/index.vue
+++ Projects/vue-blog/pages/admin/_postId/index.vue
@@ -26,3 +26,15 @@ export default {
   }
 };
 </script>
+
+<style scoped>
+.update-form {
+  width: 90%;
+  margin: 20px auto;
+}
+@media (min-width: 768px) {
+  .update-form {
+    width: 500px;
+  }
+}
+</style>
