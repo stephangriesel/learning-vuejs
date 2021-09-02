068437d9c916e84ecc7c4566dd145129cc9d1674
Stephan <sgriesel@gmail.com>
2021-09-02 06:35:24 +0200

    backend preparation

--- Projects/vue-blog/components/posts/PostList.vue
+++ Projects/vue-blog/components/posts/PostList.vue
@@ -2,18 +2,21 @@
   <section class="post-list">
     <PostPreview
       id="1"
+      v-bind:is-admin="isAdmin"
       thumbnail="https://i.stack.imgur.com/AZUmQ.jpg"
       title="Javascript"
       previewText="Yes this is Javascript"
     />
     <PostPreview
       id="2"
+      v-bind:is-admin="isAdmin"
       thumbnail="https://miro.medium.com/max/1400/1*Fp43zS64ZPoWi4IAz1n3MA.jpeg"
       title="Jquery"
       previewText="Urgh, this is Jquery"
     />
     <PostPreview
       id="3"
+      v-bind:is-admin="isAdmin"
       thumbnail="https://code.visualstudio.com/assets/docs/nodejs/vuejs/javascript-suggestions.png"
       title="VueJS"
       previewText="This is VueJS, wooohoooo"
@@ -29,6 +32,12 @@ export default {
   components: {
     PostPreview,
     PostList
+  },
+  props: {
+    isAdmin: {
+      type: Boolean,
+      default: false
+    }
   }
 };
 </script>
---
+++ Projects/vue-blog/pages/admin/_postId/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="admin-post-page">
+    <div class="update-form">
+      <AdminPostForm :post="loadedPost" />
+    </div>
+  </div>
+</template>
+
+<script>
+import AdminPostForm from "~/components/Admin/AdminPostForm.vue";
+
+export default {
+  components: {
+    AdminPostForm
+  },
+  data() {
+    return {
+      loadedPost: {
+        author: "Stephan",
+        title: "First post",
+        content: "Very nice post",
+        thumbnailLink:
+          "https://code.visualstudio.com/assets/docs/nodejs/vuejs/javascript-suggestions.png"
+      }
+    };
+  }
+};
+</script>
