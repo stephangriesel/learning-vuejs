d06b63d65ba31c5b8a8e31f88e7e7cd970167d8a
Stephan Griesel <griesel.stephan@scotch-soda.com>
2021-10-05 14:46:37 +0200

    conditional rendering

--- Practice/intro-to-vue2/main.js
+++ Practice/intro-to-vue2/main.js
@@ -4,6 +4,9 @@ var app = new Vue({
         product: 'Socks',
         desc: 'Nice shiny socks',
         image: './assets/vmSocks-green.png',
-        href: 'http://www.google.com'
+        href: 'http://www.google.com',
+        inventory: 2,
+        onStock: true,
+        onSale: true
     }
 })
