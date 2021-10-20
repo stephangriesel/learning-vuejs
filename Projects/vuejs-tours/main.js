1818a9a129df7ec36590a620ce9ad6a4a17ac6a3
Stephan <sgriesel@gmail.com>
2021-10-20 06:04:07 +0200

    move tours

---
+++ Projects/vuejs-tours/main.js
@@ -0,0 +1,43 @@
+let whatTemp = Number(prompt("What is the temperature?"))
+let app = new Vue({
+    el:"#app",
+    data: {
+        firstName:"Stephan",
+        lastName:"Griesel",
+        name: "Steph's Vue Tour",
+        title: "VueJS Tours",
+        // titleHTML: "VueJS <span class='badge'>Tours</span>",
+        alert: "This is an alert message! ",
+        tours: [
+            {title: "Rembrandt Tour", places: ["Rembrandt House", "Paint Spot", "De Waag Castle"]},
+            {title: "Tulp Tour", places: ["Tulp House", "Tulp Scene", "De Waag Castle"]},
+            {title: "Bridges Tour", places: ["Magere", "Kerk Straat", "Wall Bridge"]},
+        ], 
+        dynamicId: 'tour_section',
+        dynamicClass: 'tours',
+        // disabled: true,
+        // disabled: false, // wont render the attribute
+        //disabled: null, // wont render the attribute
+        // disabled: undefined, // wont render the attribute
+        attribute_name:"href",
+        url:"https://stephangriesel.com",
+        event_name: "click",
+        tourTitle: "Steph's Tours is on like donkey kong",
+        showTitle: true,
+        temp: whatTemp
+    },
+    methods: {
+        runFunction(){
+            console.log("test click function")
+        },
+        getFullName(){
+            return this.firstName + " " + this.lastName;
+        },
+        likeTour(index, event){
+            console.log("get event data",event);
+            const tour = this.tours[index]
+            tour.likes++
+            console.log(tour.likes)
+        }
+    }
+})
